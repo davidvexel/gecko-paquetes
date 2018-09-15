a0ac1bfc35afea6eb35f6bdf275a22816c263010
David Leal <davidvexel@gmail.com>
2018-09-14 19:47:32 -0500

    modify number of tours

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
       queryStringAuth: true
     });
 
-    fetch('https://www.geckoexcursions.com/wp-json/wp/v2/tour?tourcat=198,199')
+    fetch('https://www.geckoexcursions.com/wp-json/wp/v2/tour?tourcat=198,199&per_page=20')
       .then(response => response.json())
       .then(tours => this.setState({ tours, loading: false }));
   }
