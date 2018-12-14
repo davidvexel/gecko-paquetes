6e632c4522a160564b49daa6ec05ce8f4ad9e832
David Leal <davidvexel@gmail.com>
2018-12-14 12:38:48 -0500

    Inital commit?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { List, Card, Layout, Menu, Breadcrumb, Icon, DatePicker, Select, Avatar, Button } from 'antd';
+import { List, Card, Layout, Menu, Breadcrumb, Icon, DatePicker, Select, Avatar, Button, Row, Col, message } from 'antd';
 import './App.css';
 
 const { Meta } = Card;
@@ -19,29 +19,24 @@ class App extends Component {
       total: 0,
       subtotal: 0,
       discount: 0,
+      discountPercent: 0
     }
 
     this.updateAdultsQty = this.updateAdultsQty.bind(this);
     this.updateKidsQty = this.updateKidsQty.bind(this);
-
+    this.updateTourDate = this.updateTourDate.bind(this);
+    this.submitOrder = this.submitOrder.bind(this);
   }
 
   componentDidMount() {
     var WooCommerceAPI = require('woocommerce-api');
 
      // Woocommerce API
-     this.WooCommerce = new WooCommerceAPI({
-      url: 'https://www.geckoexcursions.com/',
-      consumerKey: 'ck_f361cf55dc849ec4b68c86631d58868aa160d19c',
-      consumerSecret: 'cs_3f700604ac4cd70af4d95ad460248a78b91978d4',
-      wpAPI: true,
-      version: 'wc/v2',
-      queryStringAuth: true
-    });
+     this.WooCommerce = '';
 
-    fetch('https://www.geckoexcursions.com/wp-json/wp/v2/tour?tourcat=198,199&per_page=20')
+    fetch('http://gecko.travelhub.test/api/tours')
       .then(response => response.json())
-      .then(tours => this.setState({ tours, loading: false }));
+      .then(tours => this.setState({ tours: tours.success, loading: false }));
   }
 
   selectTour(item) {
@@ -54,7 +49,7 @@ class App extends Component {
     // Check if item is in cart already
 
     // Make sure the tour has a WC Product related
-    if( item.meta.tour_booking_product && item.meta.tour_booking_product[0]) {
+    if( item.meta.tour_booking_product && item.meta.tour_booking_product[0] ) {
       // Get the product information
       // @TODO: Instead of fucking around with the child or adult attributes,
       // just asign the lower price to child, and highest to adults ¯\_(ツ)_/¯
@@ -71,6 +66,11 @@ class App extends Component {
           item.kidPrice = Math.min( ...prices );
           item.adultPrice = Math.max( ...prices );
 
+          // we need to push the variation id as well
+
+          /**
+           * Determine if add or remove the product
+           */
           if (! selectedTours.filter(e => e.id === item.id).length > 0 ) {
             selectedTours.push(item);
           } else {
@@ -79,11 +79,15 @@ class App extends Component {
             selectedTours.splice(index, 1);
           }
 
+          // Update state
           that.setState({selectedTours: selectedTours, listLoading: false, loading: false});
 
           that.calculateTotals();
 
         } else {
+          console.log(variations, res);
+          alert('Hubo un error al agregar este producto, por favor intenta más tarde.');
+          return;
           // product does not have 2 variations
           // return error
         }
@@ -143,7 +147,7 @@ class App extends Component {
 
       let discountAmount = (subtotalAmount/100) * discountPercent;
 
-      this.setState({subtotal: subtotalAmount, discount: discountAmount.toFixed(2), total: subtotalAmount - discountAmount});
+      this.setState({subtotal: subtotalAmount, discount: discountAmount.toFixed(2), total: subtotalAmount - discountAmount, discountPercent: discountPercent});
       
     } else {
       this.setState({subtotal: 0, discount: 0, total: 0});
@@ -151,8 +155,89 @@ class App extends Component {
     
   }
 
+  /**
+   * Validate if photo exists
+   * 
+   * @param {item} item 
+   */
+  renderPhoto(item) {
+    if ( item.image ) {
+      return 'http://gecko.travelhub.test/storage/' + item.image
+    } else {
+      return 'https://www.geckoexcursions.com/wp-content/uploads/2017/10/gecko-700x466.jpg';
+    }
+  }
+
+  /**
+   * Update tour date in object
+   * 
+   */
+  updateTourDate(date, dateString, item) {
+    const { selectedTours } = this.state;
+
+    let index = selectedTours.indexOf(item);
+
+    if(index > -1) {
+      selectedTours[index].tourDate = dateString;
+      this.setState({selectedTours});
+    }
+  }
+
+  /**
+   * Submit order
+   *
+   */
+  submitOrder() {
+    const { selectedTours, adultsQty, kidsQty, discountPercent } = this.state;
+    console.log(selectedTours, adultsQty, kidsQty, discountPercent);
+    
+    this.setState({submitLoading: true});
+    
+    // Validate data
+    if( selectedTours.length > 0 && adultsQty > 0 ) {
+
+      // Generate WooCommerce Coupon
+      if(discountPercent > 0) {
+
+        let data = {
+          code: 'paqExperiencias-' + Math.floor(Math.random() * 11),
+          discount_type: 'percent',
+          amount: discountPercent.toString(),
+          individual_use: true,
+        };
+
+        // Do the WC Call
+        this.WooCommerce.post('coupons', data, function(err, data, res) {
+          let response = JSON.parse(res);
+
+          if(response.id) {
+            message.success('Tu cupón se ha generado exitosamente, redirigiendote a tu orden.', 10);
+
+            const baseURL = 'http://geckoexcursions.com/es/cart';
+
+            window.location.href = baseURL + `?currency=mxn&coupon=${response.id}&products=123&dates=123&variations=123&adults=2&kids=0`;
+          }
+
+
+        });
+
+        this.setState({submitLoading: true});
+      } else {
+        // GO straight to the site sending the URL params
+      }
+    } else {
+      this.setState({submitLoading: true});
+      return alert('Hubo un error al crear orden, por favor intenta más tarde.');
+    }
+    // Use selectedTours and discount
+
+    
+
+    // Send to URL with ?products, variations, dates and coupon
+  }
+
   render() {
-    const { tours, loading, selectedTours, listLoading, subtotal, discount, total } = this.state;
+    const { tours, loading, selectedTours, listLoading, subtotal, discount, total, discountPercent, submitLoading } = this.state;
     return (
       <Layout className="layout">
         <Header className="header">
@@ -177,78 +262,87 @@ class App extends Component {
           </Breadcrumb>
           <div className="notice">Selecciona dos tours de esta lista y recibe un increible 20% de descuento. Selecciona 3 o más y recibe un 25% de descuento.</div>
           <Layout style={{ padding: '24px 0', background: '#fff' }}>
-            <Content className="toursList" style={{ padding: '0 24px', minHeight: 280 }}>
-              <List
-                grid={{ gutter: 16, sm: 1, md: 2, lg: 3 }}
-                dataSource={tours}
-                loading={loading}
-                renderItem={item => (
-                  <List.Item>
-                    <Card
-                      style={{ width: 300 }}
-                      cover={<img alt="example" src={item._embedded["wp:featuredmedia"][0].media_details.sizes.medium_large.source_url} />}
-                      actions={[item.meta.tour_price, <Icon type="ellipsis" />]}
-                      key={item.id}
-                      onClick={() => this.selectTour(item)}
-                      className={selectedTours.filter(e => e.id === item.id).length > 0 ? 'selected' : 'nononoo' }
-                    >
-                      <Meta
-                        title={item.title.rendered}
-                        description={unescape(item.excerpt.rendered)}
-                      />
-                    </Card>
-                  </List.Item>
-                )}
-              />
-            </Content>
-            <Sider width={300} style={{ background: '#fff' }}>
-              <List
-                dataSource={selectedTours}
-                loading={listLoading}
-                locale={{
-                  emptyText: 'Selecciona algunos tours para comenzar'
-                }}
-                renderItem={item => (
-                  <List.Item>
-                    <Card
-                      style={{width: '100%', margin: '20px'}}
-                      actions={[<span>Adulto: {item.adultPrice}</span>, <span>Menor: {item.kidPrice}</span>]}
-                    >
-                    <Meta
-                      title={item.title.rendered}
-                      description={<DatePicker placeholder="Selecciona la fecha" />}
-                      avatar={<Avatar src={item._embedded["wp:featuredmedia"][0].media_details.sizes.medium_large.source_url} />}
-                    /> 
-                    </Card>
-                  </List.Item>
-                )}
-              />
-              {selectedTours.length > 0 &&
-                <div>
-                  <Select defaultValue="2" onChange={this.updateAdultsQty} style={{width: "100%", margin: "20px"}}>
-                    <Option value="1">1 Adulto</Option>
-                    <Option value="2">2 Adultos</Option>
-                    <Option value="3">3 Adultos</Option>
-                    <Option value="4">4 Adultos</Option>
-                    <Option value="5">5 Adultos</Option>
-                  </Select>
-                  <Select defaultValue="0" onChange={this.updateKidsQty} style={{width: "100%", margin: "20px"}}>
-                    <Option value="0">Menores</Option>
-                    <Option value="1">1 Menor</Option>
-                    <Option value="2">2 Menores</Option>
-                    <Option value="3">3 Menores</Option>
-                    <Option value="4">4 Menores</Option>
-                    <Option value="5">5 Menores</Option>
-                  </Select>
+            <Row>
+              <Col lg={18} sm={24}>
+                <Content className="toursList" style={{ padding: '0 24px', minHeight: 280 }}>
+                  <List
+                    grid={{ gutter: 16, sm: 1, lg: 2, xl: 3 }}
+                    dataSource={tours}
+                    loading={loading}
+                    renderItem={item => (
+                      <List.Item>
+                        <Card
+                          style={{ width: 300 }}
+                          cover={<img alt="example" src={this.renderPhoto(item)} />}
+                          actions={[<Icon type="plus" / >]}
+                          key={item.id}
+                          onClick={() => this.selectTour(item)}
+                          className={selectedTours.filter(e => e.id === item.id).length > 0 ? 'selected' : '' }
+                        >
+                          <div className="addedIcon"><Icon type="check-circle" theme="outlined" /></div>
+                          <div className="tourPrice">
+                            <span>${ item.variations[0] && item.variations[0].price } USD</span>
+                          </div>
+                          <Meta
+                            title={item.name}
+                          />
+                        </Card>
+                      </List.Item>
+                    )}
+                  />
+                </Content>
+              </Col>
+              <Col lg={6} sm={24}>
+                <Sider width={300} style={{ background: '#fff' }}>
+                  <List
+                    dataSource={selectedTours}
+                    loading={listLoading}
+                    locale={{
+                      emptyText: 'Selecciona algunos tours para comenzar'
+                    }}
+                    renderItem={item => (
+                      <List.Item>
+                        <Card
+                          style={{width: '100%'}}
+                          actions={[<span>Adulto: {item.adultPrice}</span>, <span>Menor: {item.kidPrice}</span>]}
+                        >
+                        <Meta
+                          title={item.title.rendered}
+                          description={<DatePicker placeholder="Selecciona la fecha" showToday={false} onChange={(date, dateString) => this.updateTourDate(date, dateString, item)} />}
+                          avatar={<Avatar src={this.renderPhoto(item)} />}
+                        /> 
+                        </Card>
+                      </List.Item>
+                    )}
+                  />
+                  {selectedTours.length > 0 &&
+                    <div>
+                      <Select defaultValue="2" onChange={this.updateAdultsQty} style={{width: "100%", margin: "20px 0"}}>
+                        <Option value="1">1 Adulto</Option>
+                        <Option value="2">2 Adultos</Option>
+                        <Option value="3">3 Adultos</Option>
+                        <Option value="4">4 Adultos</Option>
+                        <Option value="5">5 Adultos</Option>
+                      </Select>
+                      <Select defaultValue="0" onChange={this.updateKidsQty} style={{width: "100%", margin: "20px 0"}}>
+                        <Option value="0">Menores</Option>
+                        <Option value="1">1 Menor</Option>
+                        <Option value="2">2 Menores</Option>
+                        <Option value="3">3 Menores</Option>
+                        <Option value="4">4 Menores</Option>
+                        <Option value="5">5 Menores</Option>
+                      </Select>
 
-                  <h2>Subtotal: ${subtotal} USD</h2>
-                  <h2 className="red">Descuento: ${discount} USD</h2>
-                  <h2>Total: ${total} USD</h2>
+                      <h2>Subtotal: ${subtotal} USD</h2>
+                      { discountPercent > 0 && <h2 className="red">Descuento ({ discountPercent }%): ${discount} USD</h2> }
+                      <h2>Total: ${total} USD</h2>
 
-                  <Button size="large" type="primary">Comprar</Button>
-                </div>
-              }
-            </Sider>
+                      <Button size="large" type="primary" loading={submitLoading} onClick={this.submitOrder}>Comprar</Button>
+                    </div>
+                  }
+                </Sider>
+              </Col>
+            </Row>
           </Layout>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
